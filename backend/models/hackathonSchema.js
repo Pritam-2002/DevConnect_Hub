50647e009bb77e1ad0016f0d3e690e709c68c192
sandip1401 <sandipgpay224@gmail.com>
2025-03-06 21:31:46 +0530

    feat-auth user api and hackathond post api done need to add image setup

---
+++ backend/models/hackathonSchema.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const hackathonSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    hackathonlink:{
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    participants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Hackathon', hackathonSchema);
