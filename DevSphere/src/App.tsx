cc14aa5ad8303a9c7271a11aeda371e1aa8d5752
jhantu626 <jhantubala626@gmail.com>
2025-03-06 17:08:08 +0530

    config: completed the configuration for fonts

--- DevSphere/src/App.tsx
+++ DevSphere/src/App.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { fonts } from './utils/fonts'
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text style={{fontFamily: fonts.bold}}>DevSphere</Text>
     </View>
   )
 }
